6858b93bcae701cd4320c30d3b7eb2350a5aac33
Taras Mankovski <tarasm@gmail.com>
2015-07-16 09:33:44 -0400

    Initial commit with example code

---
+++ app/initializers/bootstrap-payload.js
@@ -0,0 +1,18 @@
+export function initialize(container, application) {
+  var store = container.lookup('service:store'),
+    payloadKeys = Object.keys(BOOTSTRAP_DATA);
+
+  payloadKeys.forEach((key) => {
+    var registryKey = `bootstrap-payload:${key}`,
+        model;
+
+    model = store.createRecord(key, BOOTSTRAP_DATA[key]);
+    application.register(registryKey, model, {instantiate:false});
+  });
+}
+
+export default {
+  name: 'bootstrap-payload',
+  after: 'ember-data',
+  initialize: initialize
+};
